6e05418564e6657119ccd0800a552cf61706072d
Vinicius Sakai <viniciussakai0@gmail.com>
2021-12-18 17:02:58 -0300

    feat(entities): Created Lobby

---
+++ src/entities/Lobby.ts
@@ -0,0 +1,25 @@
+import { GlobalStateMap } from './GameState'
+
+export class Lobby {
+	private static lobbyState: GlobalStateMap = new Map()
+	private static rooms: Map<string, string[]> = new Map()
+
+	public addRoom(roomName): void {
+		Lobby.rooms.set(roomName, [])
+	}
+
+	public addClient(clientId, roomName): void {
+		const roomClients = Lobby.rooms.get(roomName) || []
+		Lobby.rooms.set(roomName, [...roomClients, clientId])
+	}
+
+	public getRooms = () => {
+		const rooms = Array.from(Lobby.rooms)
+		return rooms.map(([roomName, roomClients]) => {
+			return {
+				roomName,
+				capacity: roomClients.length
+			}
+		})
+	}
+}
